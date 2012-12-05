62448d4bb61ca6391bd52062d44c466865cc0437
Forrest Alvarez <forrest.alvarez@gmail.com>
2012-12-05 06:48:24 +0000

    updated setup_db to be more concise, also fixed some syntax issues, moved things to a class and functions, also added a relations table to link the podcasts and actual download links together

--- setup_db.py
+++ setup_db.py
@@ -1,4 +1,4 @@
-#!/usr/bin//python3
+#!/usr/bin/python3
 """
 Creates the database that is used by the application.
 """
@@ -14,22 +14,33 @@ class dbSetup(object):
         self.dbConn = sqlite3.connect(self.dbName)
         self.c = self.dbConn.cursor()
 
-        self.c.execute('''create table if not exists podcasts (
-                id int primary key not NULL,
-                name text,
-                url	text)''')
+        self.c.execute('''CREATE TABLE if not EXISTS podcasts (
+                id INTEGER PRIMARY KEY not NULL,
+                name TEXT,
+                url TEXT)''')
         self.dbConn.commit()
 
     # Creation function that handles setting up the downloads table.
-    def downloadLinks(self):
+    def createDownloads(self):
         self.dbConn = sqlite3.connect(self.dbName)
         self.c = self.dbConn.cursor()
-        self.c.execute('''create table if not exists downloadLib (
-                id int primary key not NULL,
-                name text,
-                url	text)''')
+        self.c.execute('''CREATE TABLE if not EXISTS downloads (
+                id INTEGER PRIMARY KEY not NULL,
+                name TEXT,
+                url TEXT)''')
+        self.dbConn.commit()
+
+    # creation function that handles setting up the relations table to
+    # link the podcasts and downloads.
+    def createRelations(self):
+        self.dbConn = sqlite3.connect(self.dbName)
+        self.c = self.dbConn.cursor()
+        self.c.execute('''CREATE TABLE if not EXISTS podcasts_downloads (
+                podcasts_id INTEGER,
+                downloads_id INTEGER)''')
         self.dbConn.commit()
 
 setup = dbSetup()
 setup.createPodcasts()
-setup.downloadLinks()
+setup.createDownloads()
+setup.createRelations()
